77f25c0a3da19c4dfcba85b10936d650376a1e62
inee1945 <inee1945@naver.com>
2024-06-10 09:17:51 +0900

    sdf

--- src/ex12inheritance/E09InstanceOf.java
+++ src/ex12inheritance/E09InstanceOf.java
@@ -3,16 +3,16 @@ package ex12inheritance;
 public class E09InstanceOf {
 
 	public static void main(String[] args) {
-		Box box1 = new Box();
-		PaperBox paperBox = new PaperBox();
-		GoldPaperBox goldPaperBox = new GoldPaperBox();
-		
-		wrapBox(box1);
-		wrapBox(box2);
-		wrapBox(box3);
-		
-		String str = new String("나는누구인가");
-		wrapBox(str);
+//		Box box1 = new Box();
+//		PaperBox paperBox = new PaperBox();
+//		GoldPaperBox goldPaperBox = new GoldPaperBox();
+//		
+//		wrapBox(box1);
+//		wrapBox(box2);
+//		wrapBox(box3);
+//		
+//		String str = new String("나는누구인가");
+//		wrapBox(str);
 	}
 
 }
